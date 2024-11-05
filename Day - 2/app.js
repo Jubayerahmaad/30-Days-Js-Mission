21a7f1167acb43a604b83c0fcd9cb7721d249648
Jubayerahmaad <jubuofficial84@gmail.com>
2024-11-05 17:47:45 +0600

    Array  & ForEach Loop

---
+++ Day - 2/app.js
@@ -0,0 +1,11 @@
+// Arrays & ForEach loop
+
+let students = ['Jubayer', 'Habib', 'Abdullah', 'Siam'];
+
+// spacipic person
+// console.log(students[2]);
+
+students.forEach(function(item){
+    console.log(item);
+})
+   
