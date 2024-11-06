25e5ac440789b73b2d78bc00b78135e1aec3f9b9
Jubayerahmaad <jubuofficial84@gmail.com>
2024-11-06 18:12:41 +0600

    Switch

--- Day - 3/app.js
+++ Day - 3/app.js
@@ -1,25 +1,58 @@
-// For loop
+// // For loop
 
-for(let i = 0; i < 10; i++){
-    console.log(i);
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+// // while loop 
+
+// let i = 5;
+
+// while(i < 10){
+//     console.log('hello word');
+//     i++;
+// }
+
+// // Oparetors 
+
+// let a = 10;
+// let b = 11;
+
+// if(a === b){
+//     console.log('Its Confirm');
+// }else{
+//     console.log('No Its Not Confirm');
+// }
+
+
+let day;
+
+switch (new Date().getDate()) {
+    case 0:
+        day = 'Sunday';
+        
+        break;
+    case 1:
+        day = 'Monday';
+        break;
+    case 2:
+        day = 'Thurday';
+        break;     
+     case 3:
+       day = 'Wedesday';
+        break; 
+     case 4:
+        day = 'Thusday';
+        break; 
+     case 5:
+        day = 'Friday';
+        break; 
+     case 6:
+        day = 'Saturday';
+        break; 
+
+    default:
+        break;
 }
 
-// while loop 
-
-let i = 5;
-
-while(i < 10){
-    console.log('hello word');
-    i++;
-}
-
-// Oparetors 
-
-let a = 10;
-let b = 11;
-
-if(a === b){
-    console.log('Its Confirm');
-}else{
-    console.log('No Its Not Confirm');
-}
+document.getElementById('demo').innerHTML = 'Today  is' + " "+ day;
