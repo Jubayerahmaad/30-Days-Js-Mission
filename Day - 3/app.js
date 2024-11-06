1882d0acd8a39b528e3922e4f2a6b1ee35f327e5
Jubayerahmaad <jubuofficial84@gmail.com>
2024-11-06 17:55:10 +0600

    For Loop , While Loop, Oparetors

---
+++ Day - 3/app.js
@@ -0,0 +1,25 @@
+// For loop
+
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
+
+// while loop 
+
+let i = 5;
+
+while(i < 10){
+    console.log('hello word');
+    i++;
+}
+
+// Oparetors 
+
+let a = 10;
+let b = 11;
+
+if(a === b){
+    console.log('Its Confirm');
+}else{
+    console.log('No Its Not Confirm');
+}
